5979eb0fe41e1edbdae465ccd63292e221d4b46a
Andres M Bran <doncamilom@gmail.com>
2025-01-06 18:26:09 +0100

    include bond_breaking class - eval specific bond breaks

--- src/steer/evaluation/eval_types/bond_break.py
+++ src/steer/evaluation/eval_types/bond_break.py
@@ -3,72 +3,88 @@ from .base import BaseScoring
 import pandas as pd
 from rdkit import Chem
 
-class SpecificBondBreak_1(BaseScoring):
+
+class SpecificBondBreak(BaseScoring):
+    def __init__(self, config):
+        if config.get("bond_to_break") is None:
+            raise ValueError("bond_to_break must be provided in config")
+        else:
+            self.atom_1 = config["bond_to_break"]["atom_1"]
+            self.atom_2 = config["bond_to_break"]["atom_2"]
+
+    def route_scoring(self, x):
+        """Disconnection happens (!=-1), + should happen late-stage roughly."""
+        if x == -1:
+            return 99
+        return x-1
+
     def hit_condition(self, d):
-        # Here, the question is when the piperidine rings become two separate molecules.
-        ring1 = 29 # N29
-        ring2 = 33 # N33
+        """Determine if the bond between A1 and A2 is broken in current reaction."""
 
         rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
         prod = Chem.MolFromSmiles(rxn[0])
         reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
 
-        if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
+        if (self.atom_1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (self.atom_2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
             for r in reacts:
-                if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
+                if (self.atom_1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (self.atom_2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
                     return True
         return False
 
-
-class SpecificBondBreak_2(BaseScoring):
-    def hit_condition(self, d):
-        # Here, the question is when the piperidine rings become two separate molecules.
-        ring1 = 17 # C17
-        ring2 = 21 # N21
-
-        rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
-        prod = Chem.MolFromSmiles(rxn[0])
-        reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
-
-        if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
-            for r in reacts:
-                if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
-                    return True
-        return False
+    def __call__(self, data):
+        return self.where_condition_met(data, self.route_scoring)
 
 
 
-class SpecificBondBreak_3(BaseScoring):
+# class SpecificBondBreak_2(BaseScoring):
+#     def hit_condition(self, d):
+#         # Here, the question is when the piperidine rings become two separate molecules.
+#         ring1 = 17 # C17
+#         ring2 = 21 # N21
 
-    def hit_condition(self, d):
-        # Here, the question is when the piperidine rings become two separate molecules.
-        ring1 = 24 # N24
-        ring2 = 26 # C26
+#         rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
+#         prod = Chem.MolFromSmiles(rxn[0])
+#         reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
 
-        rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
-        prod = Chem.MolFromSmiles(rxn[0])
-        reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
-
-        if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
-            for r in reacts:
-                if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
-                    return True
-        return False
+#         if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
+#             for r in reacts:
+#                 if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
+#                     return True
+#         return False
 
 
-class SpecificBondBreak_4(BaseScoring):
 
-    def hit_condition(self, d):
-        # Here, the question is when the piperidine rings become two separate molecules.
-        ring1 = 36 # C36
-        ring2 = 38 # C38
+# class SpecificBondBreak_3(BaseScoring):
 
-        rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
-        prod = Chem.MolFromSmiles(rxn[0])
-        reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
+#     def hit_condition(self, d):
+#         # Here, the question is when the piperidine rings become two separate molecules.
+#         ring1 = 24 # N24
+#         ring2 = 26 # C26
 
-        if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
-            for r in reacts:
-                if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
-                    return True
-        return False
+#         rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
+#         prod = Chem.MolFromSmiles(rxn[0])
+#         reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
+
+#         if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
+#             for r in reacts:
+#                 if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
+#                     return True
+#         return False
+
+
+# class SpecificBondBreak_4(BaseScoring):
+
+#     def hit_condition(self, d):
+#         # Here, the question is when the piperidine rings become two separate molecules.
+#         ring1 = 36 # C36
+#         ring2 = 38 # C38
+
+#         rxn = d['metadata']['mapped_reaction_smiles'].split(">>")
+#         prod = Chem.MolFromSmiles(rxn[0])
+#         reacts = [Chem.MolFromSmiles(r) for r in rxn[1].split(".")]
+
+#         if (ring1 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]) and (ring2 in  [a.GetAtomMapNum() for a in prod.GetAtoms()]):
+#             for r in reacts:
+#                 if (ring1 in [a.GetAtomMapNum() for a in r.GetAtoms()]) ^ (ring2 in [a.GetAtomMapNum() for a in r.GetAtoms()]):
+#                     return True
+#         return False
--- src/steer/evaluation/evaluation.py
+++ src/steer/evaluation/evaluation.py
@@ -22,8 +22,6 @@ PROMPT_TYPE = 'fullroute'
 #     gt_score, lmscore = evaluator(data, task)
 
 
-
-
 with open(f'{RESULTS_PATH}/fullroute/Early_imidazole_ring_formation.json', 'r') as f:
     data = json.load(f)
 
@@ -46,50 +44,47 @@ gt_score, lmscore = ringbreaker_depth(data)
 print(gt_score, lmscore)
 
 
+# 0...9 apply, 17...22. 25, 28, 29, 30, 32
+with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._The_disconnection_should_be_made_between_two_pipe.json', 'r') as f:
+    data = json.load(f)
+
+ev = SpecificBondBreak(config={"bond_to_break": {"atom_1": 29, "atom_2": 33}})
+gt_score, lmscore = ev(data)
+print(gt_score, lmscore)
 
 
+# With these ones we want the same, but prompt is different.
+with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._One_intermediate_will_have_piperidine,_indole_and.json', 'r') as f:
+    data = json.load(f)
 
-# def route_score(x):
-#     # Disconnection happens (!=-1), + should happen late-stage roughly
-#     if x == -1:
-#         return 99
-#     return x-1
+ev = SpecificBondBreak(config={"bond_to_break": {"atom_1": 29, "atom_2": 33}})
+gt_score, lmscore = ev(data)
+print(gt_score, lmscore)
 
-# # 0...9 apply, 17...22. 25, 28, 29, 30, 32
-# with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._The_disconnection_should_be_made_between_two_pipe.json', 'r') as f:
-#     data = json.load(f)
+with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_intermediates._The_disconnection_should_be_made_between_diazepine_and_piperazine.json', 'r') as f:
+    data = json.load(f)
 
-# ringbreaker_depth = SpecificBondBreak_1()
-# gt_score, lmscore = ringbreaker_depth.depth_score(data, target_depth=route_score)
-
-
-# # With these ones we want the same, but prompt is different.
-# with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._One_intermediate_will_have_piperidine,_indole_and.json', 'r') as f:
-#     data = json.load(f)
-
-# ringbreaker_depth = SpecificBondBreak_1()
-# gt_score, lmscore = ringbreaker_depth.depth_score(data, target_depth=route_score)
+ev = SpecificBondBreak(config={"bond_to_break": {"atom_1": 17, "atom_2": 21}})
+gt_score, lmscore = ev(data)
+print(gt_score, lmscore)
 
 
-# with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_intermediates._The_disconnection_should_be_made_between_diazepine_and_piperazine.json', 'r') as f:
-#     data = json.load(f)
+with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._The_disconnection_should_be_made_between_piperazi.json', 'r') as f:
+    data = json.load(f)
 
-# ringbreaker_depth = SpecificBondBreak_2()
-# gt_score, lmscore = ringbreaker_depth.depth_score(data, target_depth=route_score)
-
-
-# with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_that_will_cut_the_molecule_in_two_similarly_sized_intermediates._The_disconnection_should_be_made_between_piperazi.json', 'r') as f:
-#     data = json.load(f)
-
-# ringbreaker_depth = SpecificBondBreak_3()
-# gt_score, lmscore = ringbreaker_depth.depth_score(data, target_depth=route_score)
+ev = SpecificBondBreak(config={"bond_to_break": {"atom_1": 24, "atom_2": 26}})
+gt_score, lmscore = ev(data)
+print(gt_score, lmscore)
 
 
-# with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_where_the_key_disconnection_will_be_made_between_indole_and_amino-piperidine_rings.json', 'r') as f:
-#     data = json.load(f)
+with open(f'{RESULTS_PATH}/fullroute_no_feasibility/Identify_the_disconnection_strategy_where_the_key_disconnection_will_be_made_between_indole_and_amino-piperidine_rings.json', 'r') as f:
+    data = json.load(f)
 
-# ringbreaker_depth = SpecificBondBreak_4()
-# gt_score, lmscore = ringbreaker_depth.depth_score(data, target_depth=route_score)
+ev = SpecificBondBreak(config={"bond_to_break": {"atom_1": 36, "atom_2": 38}})
+gt_score, lmscore = ev(data)
+print(gt_score, lmscore)
+
+
 
 
 # with open(f'{RESULTS_PATH}/fullroute/Form_piperidine-2,6-dione_and_oxoisoindolinone_rings_in_the_retrosynthesis._Get_the_piperidine_ring_.json', 'r') as f:
