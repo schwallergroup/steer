9657c17170c533cb4404b09e91522792b8bd48ad
Andres M Bran <doncamilom@gmail.com>
2025-01-13 23:59:50 +0100

    add evaluation script for benchmark - mechanism

--- src/steer/evaluation/mechanism/evaluation.py
+++ src/steer/evaluation/mechanism/evaluation.py
@@ -8,12 +8,12 @@ from tasks import load_default_tasks
 
 from steer.logger import setup_logger
 
-logger = setup_logger()
+logger = setup_logger(__name__)
 
 path = os.path.dirname(os.path.abspath(__file__))
 
 
-def run_task(task, lm):
+async def run_task(task, lm):
     """Run a task and return the results.
     Output is list of lists. list[0] is always correct one."""
 
@@ -21,19 +21,35 @@ def run_task(task, lm):
     for i, step in enumerate(task.steps[:-1]):
         possible_moves = [step, *task.step_options[i]]
         hist = task.steps[:i]
-        scores = lm(task.rxn, hist, possible_moves)
+        response = await asyncio.gather(*[
+            lm.run(rxn=task.rxn, history=hist, step=move)
+            for move in possible_moves
+        ])
+        scores = [lm._parse_score(result) for result in response]
         results.append(scores)
     return results
 
 
-if __name__ == "__main__":
+async def main():
+    from steer.mechanism.sequential import LM
+    scorer = LM(
+        prompt="steer.mechanism.prompts.alphamol_partial",
+        model="claude-3-5-sonnet",
+        project_name="steer-mechanism-test",
+    )
+
     tasks = load_default_tasks()
-    scorer = lambda rxn, hist, moves: np.random.rand(len(moves)).tolist()
     logger.info(f"Loaded {len(tasks)} tasks.")
 
     for task in tasks:
         logger.info(f"Running task {task.id}")
-        results = run_task(task, scorer)
+        results = await run_task(task, scorer)
         gt, lm = task.evaluate(results)
         corr = np.corrcoef(gt, lm)[0, 1]
         logger.debug(f"Correlation: {corr}")
+
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
