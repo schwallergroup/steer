66d5aa40f396b21c669819668ccc443238fc7794
Andres M Bran <doncamilom@gmail.com>
2025-06-14 12:19:58 +0200

    change synthegy for aizynthfinder

--- src/steer/evaluation/synthesis/evaluation.py
+++ src/steer/evaluation/synthesis/evaluation.py
@@ -33,22 +33,6 @@ def get_latest_file(path, fid):
         return None
 
 
-# Load some sample routes
-# def cluster_routes(data: List[dict], nclusters=10):
-#     from synthegy.analysis.routes import RouteCollection
-#     from synthegy.reactiontree import ReactionTree
-
-#     routes = [ReactionTree.from_dict(d) for d in data]
-#     rc = RouteCollection(routes)
-#     index_map = rc.cluster(
-#         n_clusters=nclusters,
-#         distances_model="lstm",
-#         model_path="data/models/chembl_10k_route_distance_model.ckpt",
-#     )
-#     rts = [rc.clusters[i][0] for i in range(nclusters)]
-#     return rts, index_map
-
-
 def run_task(
     lm,
     task,
@@ -66,19 +50,6 @@ def run_task(
     with open(route, "r") as f:
         data = json.load(f)
 
-    # if nclusters:
-    #     rts, index_map = cluster_routes(data, nclusters=nclusters)
-    #     routes = [rt["reaction_tree"].to_dict() for rt in rts]
-    #     routes = asyncio.run(
-    #         lm.run_single_task(task, routes, nroutes=nclusters)
-    #     )
-
-    #     # Map back to original indices
-    #     for i, r in enumerate(data):
-    #         data[i]["lmdata"] = routes[index_map[i]]["lmdata"]
-    #     routes = data
-
-    # else:
     routes = asyncio.run(lm.run_single_task(task, data, nroutes=n))
 
     fname = os.path.join(results_path, f"{task.id}.json")
@@ -92,16 +63,3 @@ def mae(gt, lm):
         gt = [10 if x else 1 for x in gt]
     return np.mean(np.abs(np.array(gt) - np.array(lm)))
 
-
-if __name__ == "__main__":
-    pass
-    # tasks = load_default_tasks(path)
-    # mean_metric = 0
-    # for task in tasks:
-    #     result = run_task(task)
-    #     if result is not None:
-    #         # logger.info(f"{task.id}: {result[0]}, {result[1]}")
-    #         metric_val = mae(result[0], result[1])
-    #         logger.debug(f"Metric: {metric_val}")
-    #         mean_metric += metric_val
-    # logger.info(f"Mean Metric: {mean_metric / len(tasks)}")
